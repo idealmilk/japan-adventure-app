8ebf4e967f2db15e047677178f45681e11d13fbe
Matthew Gilligan <mcgilligan97@gmail.com>
2023-03-17 13:31:37 +0900

    Post pagination and new mock data

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,27 +3,69 @@ import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 import { usePostsQuery } from '../generated/graphql';
 import { Layout } from '../components/Layout';
-import { Link } from '@chakra-ui/core';
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from '@chakra-ui/core';
 import NextLink from 'next/link';
+import { useState } from 'react';
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
-    variables: {
-      limit: 10,
-    },
+  const [variables, setVariables] = useState({
+    limit: 10,
+    cursor: null as null | string,
   });
+  const [{ data, fetching }] = usePostsQuery({
+    variables,
+  });
+
+  if (!fetching && !data) {
+    return <div>Your query failed</div>;
+  }
 
   return (
     <Layout>
-      <NextLink href='/create-post'>
-        <Link>create post</Link>
-      </NextLink>
-      <div>hello world</div>
+      <Flex align='center'>
+        <Heading>Japan Adventure</Heading>
+        <NextLink href='/create-post'>
+          <Link ml='auto'>create post</Link>
+        </NextLink>
+      </Flex>
       <br />
-      {!data ? (
+      {fetching && !data ? (
         <div>loading...</div>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+        <Stack spacing={8}>
+          {data!.posts.map((p) => (
+            <Box key={p.id} p={5} shadow='md' borderWidth='1px'>
+              <Heading fontSize='xl'>{p.title}</Heading>
+              <Text mt={4}>{p.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
+      )}
+      {data && (
+        <Flex>
+          <Button
+            isLoading={fetching}
+            onClick={() => {
+              setVariables({
+                limit: variables.limit,
+                cursor: data.posts[data.posts.length - 1].createdAt,
+              });
+            }}
+            m='auto'
+            my={8}
+          >
+            Load more
+          </Button>
+        </Flex>
       )}
     </Layout>
   );
